ec50d951a2a7f58b0670bf80469f5d1b38fd1a57
Hiroaki Kadomatsu <note103@gmail.com>
2015-02-12 01:25:57 +0900

    if文で分岐することに

--- overlap.pl
+++ overlap.pl
@@ -1,19 +1,24 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use utf8;
 use feature 'say';
+
+my $switch = 'o';
+#my $switch = 'u';
 
 my %count;
 for (<DATA>) {
     $count{$_}++;
 }
-
-my @overlap = grep {$count{$_} >= 2} keys %count;
-#say "overlap:";
-#say @overlap;
-
-my @unique = grep {$count{$_} == 1} keys %count;
-say "unique:";
-say @unique;
+if ($switch eq 'o') {
+    my @overlap = grep {$count{$_} >= 2} keys %count;
+    say "overlap:";
+    say @overlap;
+} elsif ($switch eq 'u') {
+    my @unique = grep {$count{$_} == 1} keys %count;
+    say "unique:";
+    say @unique;
+}
 
 __DATA__
