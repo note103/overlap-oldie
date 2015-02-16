e4be2e82b3bd7c1911d813aef9503046c0dfe447
Hiroaki Kadomatsu <note103@gmail.com>
2015-02-16 23:07:46 +0900

    Add ja version

--- week.pl
+++ week.pl
@@ -5,6 +5,11 @@ use feature 'say';
 use Time::Piece;
 use Time::Seconds;
 
+#my $switch = 'Sun';
+#my $switch = 'Sunday';
+#my $switch = '日';
+my $switch = '日曜日';
+
 my ($stamp_local, $stamp_day);
 my @week = <DATA>;
 for (@week) {
@@ -12,8 +17,15 @@ for (@week) {
     my $output = $_;
     $_ =~ s/\//-/g;
     $stamp_local = localtime->strptime($_, '%Y-%m-%d');
-    #$stamp_day = $stamp_local->fullday; #Sunday
-    $stamp_day = $stamp_local->wdayname; #Sun
+    if ($switch eq 'Sun') {
+        $stamp_day = $stamp_local->fullday;
+    } elsif ($switch eq 'Sunday') {
+        $stamp_day = $stamp_local->wdayname;
+    } elsif ($switch eq '日') {
+        $stamp_day = $stamp_local->wdayname(qw{日 月 火 水 木 金 土});
+    } elsif ($switch eq '日曜日') {
+        $stamp_day = $stamp_local->wdayname(qw{日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日});
+    }
     say $output."\t".$stamp_day;
 }
 
